e2a6f741b4409c441687da5ed56124b57ac3a66c
MADANA-GS <madangsnaik@gmail.com>
2026-03-17 08:43:27 +0530

    feat: integrate Redis for metric storage and update addMetric function for data handling

--- backend/controlers/metric.controler.js
+++ backend/controlers/metric.controler.js
@@ -1,4 +1,7 @@
-export const addMetric = (req, res) => {
+import { pubClient } from "../config/redis.js";
+import { io } from "../server.js";
+
+export const addMetric = async (req, res) => {
   try {
     const data = req.body;
 
@@ -33,20 +36,76 @@ export const addMetric = (req, res) => {
       0.2 * battery +
       0.1 * connectivity;
 
-    // {
-    //   voltage;
-    //   current;
-    //   power;
-    //   expected_power;
-    //   efficiency;
-    //   health_score;
-    //   temperature;
-    //   irradiance;
-    //   trust_score;
-    //   timestamp;
-    // }
+    const cachedValueFromRedis = await pubClient.hget(
+      `userId:${userId}:deviceId:${deviceId}`,
+    );
+
+    let parsed;
+
+    if (cachedValueFromRedis) {
+      parsed = JSON.parse(cachedValueFromRedis);
+
+      const { deviceId, userId, metric } = parsed;
+      metric.push({
+        voltage,
+        current,
+        power,
+        expected_power: expectedPower,
+        efficiency,
+        health_score: healthScore,
+        temperature,
+        irradiance,
+        trust_score,
+        timestamp,
+        panelCapacity,
+      });
+
+      await pubClient.hset(
+        `userId:${userId}:deviceId:${deviceId}`,
+        JSON.stringify(parsed),
+      );
+    } else {
+      let key = `userId:${userId}:deviceId:${deviceId}`;
+
+      let payload = {
+        deviceId,
+        userId,
+        metric: [],
+      };
+
+      payload.metric.push({
+        voltage,
+        current,
+        power,
+        expected_power: expectedPower,
+        efficiency,
+        health_score: healthScore,
+        temperature,
+        irradiance,
+        trust_score,
+        timestamp,
+        panelCapacity,
+      });
+
+      await pubClient.hset(key, JSON.stringify(payload));
+    }
 
     // need to store this in redis
+
+    let currentMetricPayload = {
+      voltage,
+      current,
+      power,
+      expected_power: expectedPower,
+      efficiency,
+      health_score: healthScore,
+      temperature,
+      irradiance,
+      trust_score,
+      timestamp,
+      panelCapacity,
+    };
+    io.to(userId).emit("metric", currentMetricPayload);
 
     return res.status(200).json({
       message: "Collected the data successfully",
